7d1e268d7ad89ef95d11a295e4ae0ffbbb101f96
AlexDavidJJ <alex.j@koderstribe.com>
2026-03-13 14:06:22 +0530

    added a blank line for improved code readability

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeadlinePage from './pages/HeadlinePage'
 import {  Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
 
+
 const App = () => {
   return (
     <>
