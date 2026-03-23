77b01e0f4f8bcbe2646d2874c05fb15c5383833e
balap-wq <bala.p@koderstribe.com>
2026-03-23 15:57:31 +0530

    Completed the postgres setup in Node.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import app from "./app";
+import { testConnection } from "./config/db";
 
 const PORT = 5000;
+
+testConnection();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
