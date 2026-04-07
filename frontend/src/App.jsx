748b7be593d971b6401100b7f8f41667d95e6844
Balaji-140304 <balaji.s@Koderstrib.com>
2026-04-07 12:18:59 +0530

    bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import HeadlinePage from './pages/HeadlinePage';
 import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
-import Articledetailpage from './Components/ArticleDetailpage';
+import Articledetailpage from './Components/ArticleDetailPage';
 
 const App = () => {
   return (
