6be724786f3d8e325ceb66a4c093db9aab4e73e7
Balaji-140304 <balaji.s@Koderstrib.com>
2026-04-13 15:03:24 +0530

    improved ui using lucid react

--- frontend/src/pages/HeadlinePage.jsx
+++ frontend/src/pages/HeadlinePage.jsx
@@ -34,7 +34,7 @@ const HeadlinePage = () => {
   return (
     <div>
       <div className="mx-auto w-full text-center mt-7 flex items-center justify-center gap-2">
-        <TrendingUp size={40}/>
+        <TrendingUp size={40} />
         <h1 className="text-3xl font-bold">Top Headlines</h1>
       </div>
 
