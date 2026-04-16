e06f5f5fafe578ff09d430d2c66eb1816e25cfeb
narem-lang <naren.m@koderstribe.com>
2026-04-16 15:24:51 +0530

    ui changes in the css

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,9 +1,19 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
   js.configs.recommended,
+
+  // Disable ESLint rules that conflict with Prettier
+  prettierConfig,
+
   {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+
     languageOptions: {
       globals: {
         ...globals.node,
@@ -11,12 +21,16 @@ export default [
       ecmaVersion: 2022,
       sourceType: 'module',
     },
+
     rules: {
       'no-console': 'off',
       'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       eqeqeq: ['error', 'always'],
       'no-var': 'error',
       'prefer-const': 'error',
+
+      // 🔥 THIS LINE CONNECTS PRETTIER
+      'prettier/prettier': 'error',
     },
   },
 ];
