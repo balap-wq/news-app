05f11d0dbd83072e744e8c7e69c3a1eb9ba19980
DhetchanaMoorthi <dhetchana.r@koderstribe.com>
2026-04-20 17:34:38 +0530

    update dependencies and improve code formatting across multiple files

--- backend/prisma/migrations/20260415064412_make_user_optional/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "articles" DROP CONSTRAINT "articles_userId_fkey";
-
--- AlterTable
-ALTER TABLE "articles" ALTER COLUMN "userId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "articles" ADD CONSTRAINT "articles_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
