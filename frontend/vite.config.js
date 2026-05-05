0cfa31215f371b189329ef14f06fb34bf72dbefd
Balaji-140304 <balaji.s@Koderstrib.com>
2026-05-05 14:53:30 +0530

    Fixed axiosinstance to switch between relative api path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss({
-      config: './tailwind.config.js',
-    }),
-  ],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // ✅ IMPORTANT FIX
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss({
+        config: './tailwind.config.js',
+      }),
+    ],
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL, // ✅ correct
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
